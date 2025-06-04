95946f362af666650e89b335b74bdabd9da86c00
kand417 <kand417@gmail.com>
2025-06-04 11:26:41 +0900

    ページタイトルを変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "kand417のWebページ",
+  DESCRIPTION: "kand417のWebページへようこそ！",
+  EMAIL: "",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
